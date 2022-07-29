38a9120bb42807dd78a936fc47a787036f04b7b2
nunosilva7 <40190144@esmad.ipp.pt>
2022-07-29 21:05:23 +0100

    a

--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -18,7 +18,7 @@ import seedDatabase from "./seeders";
 // passing the database connection parameters
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT as Dialect,
+    dialect: 'mysql' as Dialect,
 	pool: {
 		max: 500,
 		min: 0,
